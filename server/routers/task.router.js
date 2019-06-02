0ca1b57377ba4ba2b75bb377722e846946c3e6b7
John Gellert <gellertjm@gmail.com>
2019-06-02 15:15:38 -0500

    add a post route to task.router.js. tested with postman

--- server/routers/task.router.js
+++ server/routers/task.router.js
@@ -2,10 +2,15 @@ const express = require('express'); // import express module
 const router = express.Router(); // declare router and set to express router
 
 // declare taskList and set to import from module task.js
+// taskList is an array of objects
 const taskList = require('../modules/task.js');
 
 router.get('/', (req,res) => {
     res.send(taskList);
-})
+});
+
+router.post('/', (req, res) => {
+    res.sendStatus(201);
+});
 
 module.exports = router;
