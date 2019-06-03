b05d2a98966abcbaf9bae78ce18b8d85a0623412
John Gellert <gellertjm@gmail.com>
2019-06-02 21:17:51 -0500

    add handleClickAddTask functin to client.js

--- server/public/client.js
+++ server/public/client.js
@@ -1,5 +1,31 @@
 $(document).ready(readyNow);
 
 function readyNow(){
-    // $('#add-task').on('click', handleClickAddTask);
+    $('#add-task').on('click', handleClickAddTask);
+    $('#todo-list').on('click', '.complete-button', handleClickCompleteTask);
+    handleClickAddTask();
+}
+
+function handleClickAddTask () {
+    console.log("add");
+    $.ajax({
+        method: 'GET',
+        url: '/tasks',
+    }).then( (response) => {
+        $('#todo-list').empty();
+        response.forEach(task => {
+            $('#todo-list').append(`
+                <tr>
+                    <td>${task.task}</td>
+                    <td>${task.priority}t</td>
+                    <td><button class="complete-button">Complete</button></td>
+                </tr>
+            `);
+
+        });
+    });
+}
+
+function handleClickCompleteTask () {
+    console.log("complete");
 }
